35a4881d611e1e36d408a87e426e568997d9199b
Konstantinos <konstantinosgkotzamanidis@outlook.com>
2018-10-23 20:49:33 +0300

    Add Character Markt and fix some issue

--- dist/DBInstallerGS.sh
+++
@@ -1,1 +0,0 @@
-java -Dfile.encoding=UTF8 -Xms128m -Xmx128m -cp ../gameserver/lib/*;L2jCore.jar com.l2j.tools.dbinstaller.LauncherGS
--- dist/DBInstallerLS.bat
+++ dist/DBInstallerLS.bat
@@ -1,3 +1,4 @@
 @echo off
-title L2jLastWarrior GameServer Database Installer
-java -Dfile.encoding=UTF8 -Xms128m -Xmx128m -cp ../gameserver/lib/*;L2jCore.jar com.l2j.tools.dbinstaller.LauncherLS
+title Account Manager
+@java -Djava.util.logging.config.file=console.cfg -cp ../gameserver/lib/*;lib/L2jCore.jar com.l2j.tools.dbinstaller.LauncherLS
+@pause
