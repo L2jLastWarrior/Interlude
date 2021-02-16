619765050e8f0c8a93363d39f7ce8e863e7ead51
kgkotzamanidis <konstantinos@SatelliteC660D>
2021-02-16 11:49:43 +0200

    Bug fix in account's sql at recovery_key

--- sql/login/accounts.sql
+++ sql/login/accounts.sql
@@ -1,6 +1,3 @@
--- ---------------------------
--- Table structure for accounts
--- ---------------------------
 CREATE TABLE IF NOT EXISTS `accounts` (
   `login` VARCHAR(45) NOT NULL default '',
   `password` VARCHAR(45) ,
@@ -9,6 +6,6 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `lastIP` VARCHAR(20),
   `lastServer` int(4) default 1,
   `email` VARCHAR(45),
-  `recoveypin` int(6),  
+  `recovery_key` int(6),  
   PRIMARY KEY (`login`)
 );
